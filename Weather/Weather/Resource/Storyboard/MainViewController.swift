a6d172b8f375f9064f2c5a513dbb5b063439257f
lee-euijin <leeuj9663@naver.com>
2022-06-21 16:11:33 +0900

    [CHORE] #1 폴더링 세팅

---
+++ Weather/Weather/Resource/Storyboard/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by 이의진 on 2022/06/21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
